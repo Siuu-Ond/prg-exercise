7339f5243c4434abc6bdf7c50667a82d7d1c82b5
Ondřej Siuda <283194@vutbr.cz>
2026-04-20 13:34:00 +0200

    Bubble sort

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -14,11 +15,32 @@ def selection_sort(rada_numbers_original):
                 min_ind = index
                 min_val = rada_numbers[index]
         rada_numbers[min_ind], rada_numbers[min_index] = rada_numbers[min_index], rada_numbers[min_ind]
-    simple_sol = sorted(rada_numbers)
-    print(f"jednoduché řešení sorted: {simple_sol}")
+
+def bubble_sort(rada_numbers_original):
+    rada_numbers = rada_numbers_original.copy()
+    max_index = len(rada_numbers)
+    plt.ion()
+    plt.show()
+
+    for i in range(max_index):
+
+        for current_index in range(max_index - 1 - i):
+            if rada_numbers[current_index] > rada_numbers[current_index + 1]:
+                rada_numbers[current_index], rada_numbers[current_index + 1] = rada_numbers[current_index + 1], rada_numbers[current_index]
+
+            index_highlight1 = current_index
+            index_highlight2 = current_index + 1
+            colors = ["steelblue"] * len(rada_numbers)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(rada_numbers)), rada_numbers, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
 
 
-randomm = random_numbers(10)
-selection_sort(randomm)
 
+randomm = random_numbers(200)
+# selection_sort(randomm)
+bubble_sort(randomm)
 
